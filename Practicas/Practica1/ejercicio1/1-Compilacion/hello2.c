57268edfdc3a45dd0b4a25881a9e063a2ba0a4dd
Laura Gomez <lgomez25@ucm.es>
2023-05-31 12:48:15 +0200

    Practica1/Ejercicio 1
    1-Compilación

--- Practicas/Practica1/ejercicio1/1-Compilacion/hello2.c
+++ Practicas/Practica1/ejercicio1/1-Compilacion/hello2.c
@@ -2,11 +2,20 @@
 
 /******** TAREAS *****
 * 1. Compila el siguiente código y ejecútalo
+    + Para compilar: g++ -o (nombre del ejecutable) <nombre del archivo>.c
+
 * 2. Posteriormente, obtén la salida de la etapa de pre-procesado (opción -E o
 *    la opción --save-temps para obtener la salida de todas las etapas
 *    intermedias) y en un fichero hello2.i
+    + -E lo escribe todo en la consola, --save-temps lo genera en archivos .i .o .s
+
 * 3. ¿Qué ha ocurruido con la "llamada a min()" en hello2.i?
+    + Ha cambiado la llamada a min() por su definición que está en la segunda línea del archivo 
+    min()->  (a<b)?a:b 
+
 * 4. ¿Qué efecto ha tenido la directiva #include <stdio.h>?
+    + Se ha copiado todo el código del archivo stdio.h al archivo que lo incluye. Se ha puesto todo el código
+    encima del código de hello2.c
 *****************/
 
 #define N 5
@@ -16,7 +25,8 @@ int a = 7;
 int b = 9;
 int main() {
 
- char* cad = "Hello world";
+//Cambiado a const char para evitar el warning:  warning: ISO C++ forbids converting a string constant to ‘char*’ [-Wwrite-strings]
+const char* cad = "Hello world";
  int i;
 
  for (i=0;i<N;i++) {
