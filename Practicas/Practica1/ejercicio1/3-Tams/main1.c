838a52ba029a1da0d89ed6373775849e6cd91622
Laura Gomez <lgomez25@ucm.es>
2023-05-31 14:12:39 +0200

    Practica1/Ejercicio 1
    3-Tams

--- Practicas/Practica1/ejercicio1/3-Tams/main1.c
+++ Practicas/Practica1/ejercicio1/3-Tams/main1.c
@@ -2,10 +2,21 @@
 /**** TAREAS ********
  * Compila el codigo, ejecutalo y responde a las cuestiones
  * 1. ¿Por qué el primer printf() imprime valores distintos para 'a'?
+ 		+ Se imprimen valores diferentes porque en el printf se indica que se deben imprimir diferentes tipos de variable
+		%d indica que se debe imprimir un entero
+		%c indica que se debe imprimir un char
+		122 equivale al char 'z'.
+
  * 2. ¿Cuánto ocupa un tipo de datos char?
- * 3. ¿Por qué el valor de 'a' cambia tanto al incrementarlo en 6?
- * (la  respuesta está relacionada con la cuestión anterior)
+		+ Un char ocupa 1 byte como se indica en la cuarta linea que imprime el ejecutable
+
+ * 3. ¿Por qué el valor de 'a' cambia tanto al incrementarlo en 6? (la  respuesta está relacionada con la cuestión anterior)
+		+ El cambio se debe a que un char ocupa 1 byte que corresponde a 127 posibles valores positivos, por lo que al cambiar a 
+		a 128 no encuentra ningún valor de char correspondiente.
+ * 
  * 4. Si un "long" y un "double" ocupan lo mismo, ¿por qué hay 2 tipos de datos diferentes?
+ * 		+ Son diferentes porque long se refiere a valores de tipo entero que pueden ir de negativos enteros muy pequeños a 
+ * 		positivos muy grandes; en cambio double se refiere a variables de tipo decimal más preciso que float.
  */
 
 char a = 122;
@@ -14,7 +25,7 @@ int b = 41;
 int main()
 {
 	printf("a = %d a = %c \n", a, a);
-	a += 6;
+	a += 7;
 	printf("a = %d a = %c b=%d  b=%c\n", a, a, b, b);
 	printf("Size of int: %lu\n", sizeof(int) );
 	printf("Size of char: %lu\n", sizeof(char) );
--- Practicas/Practica1/ejercicio1/3-Tams/main2.c
+++ Practicas/Practica1/ejercicio1/3-Tams/main2.c
@@ -4,14 +4,36 @@
 /******** TAREAS *****
  * Compila el código y trata de ejecutarlo. Contesta a las cuestiones
  * 1. ¿Tenemos un problema de compilación o de ejecución?
+ * 		+ Hay un problema de compilación, ya que un error en el código impide que se generen los archivos de compilación
+ 
  * 2. ¿Por qué se da el problema?. Soluciónalo, compila y ejecuta de nuevo.
+		+ El problema se da porque hay un array inicializado con el tamaño de una variable no constante, que además cambia en el código.
+		Para arreglarlo, he puesto directamente el valor de la variable a al inicializar el vector array2
+		int array2[a] -> int array2[7]
+
  * 3. a,b,c, y x están declaradas de forma consecutiva. ¿Sus direcciones son concesutivas?
- * 4. ¿Qué significa el modificar "%lu" en printf()?
+	+ Las direcciones son no consecutivas, como se puede observar en la salida de la ejecución.
+	Por ejemplo la dirección de a dista en 8 bytes de la de a, pero a solo ocupa 4 bytes.
+
+ * 4. ¿Qué significa el modificador "%lu" en printf()?
+	+ %lu se utiliza para imprimir el valor de una variable de tipo long int 
+
  * 5. ¿A qué dirección apunta "pc"? ¿Coincide con la de alguna variable anteriormente declarada? Si es así,
  * ¿Coinciden los tamaños de ambas?
- * 6. ¿Coincide el valor del tamaño de "array1" con el número de elementos del array? ¿Por qué?
+ * 	+ pc apunta a la dirección 0x55e482c69088, que es la dirección en la que está almacenada la variable x,
+ *  pero esta tiene tamaño 1 y pc tiene tamaño 8
+ * 
+ * 6. ¿Coincide el valor del tamaño de "array1" con el número de elementos del array? ¿Por qué
+ * 		+ Los valores no coinciden porque el tamaño del array se refiere a la cantidad absoluta de 
+ * 		bytes que almacena, como se puede observar en el código, el array tiene tamaño 10 y almacena variables
+ * 		de tipo int, que ocupan 4 bytes, por tanto el tamaño total es de 40 bytes.
+ * 
  * 7. ¿Coinciden las direcciones a la que apunta cadena1 con la de cadena2?
+ * 		+ No, apuntan a direcciones completamente diferentes
+ * 
  * 8. ¿Por qué los tamaños (según sizeof()) de cadena1 y cadena2 son diferentes?
+ * 		+ Cadena1 se declara como un puntero a una cadena y cadena2 se declara directamente como array, 
+ * 		por lo que sizeof de la primera cadena devuelve el tamaño del puntero y sizeof de la segunda devuelve el tamaño del array
 ***************/
 
 #define ARRAY_SIZE  10
@@ -23,14 +45,13 @@ char x;
 char* pc;
 
 int array1[ARRAY_SIZE];
-int array2[a];
+int array2[7];
 
 char* cadena1 = "CADENA DE CARACTERES";
 char cadena2[] = "CADENA DE CARACTERES";
 int main()
 {
 	pc = &x;
-	a = 16;
 	printf("Direccion de a: %p Tam: %lu \n",&a,sizeof(a));
 	printf("Direccion de b: %p Tam: %lu \n",&b,sizeof(b));
 	printf("Direccion de c: %p Tam: %lu \n",&c,sizeof(c));
