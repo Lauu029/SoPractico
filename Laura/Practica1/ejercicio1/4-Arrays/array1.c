5093cc75a2ac6cfea84831c39a96db90bc08ccb8
Laura Gomez <lgomez25@ucm.es>
2023-06-03 13:34:21 +0200

    Practica1/Ejercicio 1
    4-Arrays

--- Laura/Practica1/ejercicio1/4-Arrays/array1.c
+++ Laura/Practica1/ejercicio1/4-Arrays/array1.c
@@ -3,11 +3,26 @@
 /************** TAREAS********
  * Compila y ejecuta el código. Contesta a las cuestiones
  * 1. ¿Por qué no es necesario escribir "&list" para obtener la dirección del array list?
+ * 		+ Cuando se utiliza el nombre de un array en un contexto en el que se espera una
+ * 		dirección de memoria, el nombre del array se convierte de forma implícita en un puntero al primer
+ * 		elemento del array.
+ * 
  * 2. ¿Qué hay almacenado en la dirección de "list"?
+ * 		+ La dirección de list conytiene la ubicación de memoria del primer elemento del array.
+ * 
  * 3. ¿Por qué es necesario pasar como argumento el tamaño del array en la función init_array?
+ * 		+ Porque init_array recibe un puntero a un array y el tamaño de dicho array, pero el puntero no 
+ * 		tiene información de cual es el tamaño del array, solo cual es su dirección de memoria
+ * 
  * 4. ¿Por qué el tamaño (sizeof())  del array de la función "init_array" no coincide con el declarado en main()?
+ * 		+ Porque sizeof(array) devuelve el tamaño del puntero al array
+ * 
  * 5. ¿Por qué NO es necesario pasar como argumento el tamaño del array en la función init_array2?
+ * 		+ Poruqe la función init_array2 obtiene como argumento un array de tamaño fijo N, por lo que se conoce
+ * 		el tamaño del array en tiempo de compilación.
+ * 
  * 6. ¿Coincide el  tamaño (sizeof())  del array de la función "init_array2" con el declarado en main()?
+ * 		No, porque sizeof devuelve el tamaño del puntero al array
  ***************/
 
 #define N 5
@@ -34,8 +49,10 @@ int main(void)
 
 void init_array(int array[], int size)
 {
+	//He cambiado el valor de sizeof de array a &array, que sigue devolviendo el valor del puntero de array
+	//pero evita los warnings
 	int i;
-	printf("Direccion de array: %p Sizeof array %lu \n", array, sizeof(array));
+	printf("Direccion de array: %p Sizeof array %lu \n", array, sizeof(&array));
 	for (i = 0; i < size; i++)
 		array[i] = i;
 	printf("Array initialized\n\n");
@@ -44,7 +61,7 @@ void init_array(int array[], int size)
 void init_array2(int array[N])
 {
 	int i;
-	printf("Direccion de array: %p Sizeof array %lu \n", array, sizeof(array));
+	printf("Direccion de array: %p Sizeof array %lu \n", array, sizeof(&array));
 	for (i = 0; i < N; i++)
 		array[i] = i*2;
 	printf("Array initialized\n\n");
--- Laura/Practica1/ejercicio1/4-Arrays/array2.c
+++ Laura/Practica1/ejercicio1/4-Arrays/array2.c
@@ -1,57 +1,73 @@
 #include <stdio.h>
-
+#include <string.h>
 /************ TAREAS
  * Compila y ejecuta el código. Contesta a las cuestiones:
  *  1. ¿La copia del array se realiza correctamente? ¿Por qué?
+ * 		+ No se copia el array de forma correcta porque la función que "copia los arrays" simplemente
+ * 		se está modificando la dirección a la que apunta dst.
+ *
  *  2. Si no es correcto, escribe un código que sí realice la copia correctamente.
+ *		+ Hay dos formas: con memcpy o con un for que recorra los elementos y los iguale uno a uno
+
  *  3. Descomenta la llamada a la fución "tmo" en la función main(). Compila de nuevo y ejecuta.
  *  4. El problema que se produce, ¿es de compilación o de ejecución? ¿Por qué se produce?
+ * 		+ Se produce un error de ejecución porque intenta acceder a posiciones fuera de los límites del array
+ * 
  *  5. Encuentra un valor de MAXVALID superior a 4 con el que no se dé el problema. ¿Se está escribiendo
  *     más allá del tamaño del array? Si es así, ¿por qué funciona el código?
-*******************/
+ * 		+ El valor máximo para que no de error es 6, está sobreescribiendo posiciones de memoria que están fuera del límite del array, 
+ * 		en este caso no da problemas porque son posiciones de memoria que están dentro de la pila, por tanto existen en el contexto 
+ * 		de la ejecución.
+ *******************/
 
 #define N 10
 #define MAXELEM 5000
-#define MAXVALID 100
+#define MAXVALID 6
 
 // funcion que imprime por pantalla el conteniod del array v de tam. size
-void imprimeArray(int v[],int size)
+void imprimeArray(int v[], int size)
 {
 	int i;
 	printf("-------------------\n");
-	for (i=0;i<size;i++)
-		printf("%d ",v[i]);
+	for (i = 0; i < size; i++)
+		printf("%d ", v[i]);
 	printf("\n\n");
 }
 
 // funcion que copia el contenido de un array en otro
-void copyArray(int src[],int dst[],int size)
+void copyArray(int src[], int dst[], int size)
 {
- 	dst = src;
+	// Este código no realiza la copia correctamente: dst = src;
+	// Código corregido:
+	/*for (int i = 0; i < size; i++)
+	{
+		dst[i] = src[i];
+	}*/
+
+	// También se puede utilizar con memcpy, pero hay que añadir la biblioteca string:
+	memcpy(dst, src, size * sizeof(int));
 }
 
-/*
 void tmo()
 {
 	int x = -1;
-	int a[4] = {0,1,2,3};
+	int a[4] = {0, 1, 2, 3};
 	int b = 10000;
 	int c = -1;
 	int i;
 
-	for  (i=4;i<MAXVALID;i++)
-		a[i]=i;
+	for (i = 4; i < MAXVALID; i++)
+		a[i] = i;
 
-	printf("x %d b %d c %d\n", x,b,c);
+	printf("x %d b %d c %d\n", x, b, c);
 }
-*/
 
 int main()
 {
-	int A[N] = {4,3,8,5,6,9,0,1,7,2};
+	int A[N] = {4, 3, 8, 5, 6, 9, 0, 1, 7, 2};
 	int B[N];
 
-	//tmo();
-	copyArray(A,B,N);
-	imprimeArray(B,N);
+	tmo();
+	copyArray(A, B, N);
+	imprimeArray(B, N);
 }
