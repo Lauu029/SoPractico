96fcd1e5ccad51ba37a3af968c85551a0992f8a2
Laura Gomez <lgomez25@ucm.es>
2023-06-03 19:09:30 +0200

    Practica1/Ejercicio 1
    Completo

--- Laura/Practica1/ejercicio1/7-Strings/strings1.c
+++ Laura/Practica1/ejercicio1/7-Strings/strings1.c
@@ -4,20 +4,33 @@
 /********* TAREAS ************
  * Compila y ejecuta el código. Responde a las siguientes cuestiones:
  * 1. El código contiene un error. ¿Se manifiesta en compilación o en ejecución? ¿Por qué se produce?
- * Soluciona el error comentando la(s) línea(s) afectadas. Vuelve a compilar y ejecutar.
+ * 		+ Se manifiesta un error en ejecución  no se ha puesto un caracter de finalización al mensaje by y
+ * 		está escribiendo valores sin asignar del puntero msg.
+ *
+ * Soluciona el error comentando la(s) línea(s) afectadas. Vuelve a compilar y ejecutar.(añadido msg[2]='\0';)
+ * 
  * 2. ¿En qué dirección está la letra 'B' de la cadena Bonjour? ¿Y la de la la letra 'j'?
+ * 		+ La b, al ser la primera letra del puntero está en la dirección a la que apunta, es decir 0x55c1d2fe7008,
+ * 		la letra j está sumando 3 posiciones o bytes(ya que cada char ocupa un byte) a la posición a la que apunta p
+ * 
  * 3. Tras la asignación p=msg2;, ¿cómo podemos recuperar la dirección de la cadena "Bonjour"?
+ *		+ No se puede recuperar, es memoria perdida.
+
  * 4. ¿Por qué la longitud de las cadenas p y msg2 es 2 tras la línea 30?
  *    Se asignan 3 bytes a 'p' que modifican a ambos, pero luego la longitud es sólo 2.
+ * 		+ El último byte ('\0') es un byte nulo que no cuenta al coger la longitud de la palabra, ya que solo se 
+ * 		utiliza para indicar el final de la palabra.
+ * 
  * 5. ¿Por qué strlen() devuelve un valor diferente a sizeof()?
+ * 		+ Porque strlen devuelve la cantidad de letras que tiene la palabra y sizeof su tamaño en bytes en memoria.
  ************** */
 int main()
 {
-	char msg[10]; /* array of 10 chars */
-	char *p;      /* pointer to a char */
-	char msg2[28] = "Hello";  /* msg2 = 'H' 'e' 'l' 'l' 'o' '\0' */
+	char msg[10];			 /* array of 10 chars */
+	char *p;				 /* pointer to a char */
+	char msg2[28] = "Hello"; /* msg2 = 'H' 'e' 'l' 'l' 'o' '\0' */
 
-	p   = "Bonjour";
+	p = "Bonjour";
 	printf("msg: %s, p: %s, msg2: %s\n", msg, p, msg2);
 	printf("dir de msg: %p, dir de p: %p, dir de msg2: %p\n", msg, p, msg2);
 
@@ -30,7 +43,6 @@ int main()
 	printf("msg len: %lu p len %lu msg2 len %lu\n", strlen(msg), strlen(p), strlen(msg2));
 	printf("msg size: %lu p size %lu msg2 size %lu\n", sizeof(msg), sizeof(p), sizeof(msg2));
 
-	msg[0] = 'B', msg[1] = 'y';
+	msg[0] = 'B', msg[1] = 'y', msg[2] = '\0';
 	printf("msg: %s, p: %s, msg2: %s\n", msg, p, msg2);
 }
-
--- Laura/Practica1/ejercicio1/7-Strings/strings2.c
+++ Laura/Practica1/ejercicio1/7-Strings/strings2.c
@@ -4,34 +4,38 @@
 /********* TAREAS ************
  * Compila y ejecuta el código. Responde a las siguientes cuestiones:
  * 1. El código de "copia" no funciona. ¿Por qué?
- * 2. Usa ahora la función copia2() (descomenta la línea correspondiente). ¿Funciona la copia?
+ * 		+ Dst cambia en el contexto de la función copia, pero fuera de esta el valor no ha cambiado, ya que no se ha pasado por referencia
+ *
+ * 2. Usa ahora la función copia2() (descomenta la línea correspondiente). ¿Funciona la copia? Si:)
  * 3. Propón una implementación correcta de la copia.
  * 4. ¿Qué hace la funcióbn "mod"? ¿Por qué funciona?
+ * 		+ Iguala cada elemento de un char al otro pero con el valor ascii correspondiente a las mayúsculas
  * 5. Descomenta la última llamada a la función "mod". Compila y ejecuta. ¿Por qué se produce el error?
+ * 		+ Intenta acceder a direcciones de memoria que no están en la pila del programa.
  ************** */
-
-void copia2(char* org, char** dst)
+// char ** pasa el puntero por referencia
+void copia2(char *org, char **dst)
 {
 	*dst = org;
 }
-
-void copia(char* org, char* dst)
+// copia modificada
+void copia(char *org, char *dst)
 {
 	dst = org;
 }
 
-void mod(char* org, char* dst)
+void mod(char *org, char *dst)
 {
 	int i;
 
-	for (i=0;i<strlen(org);i++)
+	for (i = 0; i < strlen(org); i++)
 		dst[i] = org[i] - 32;
 }
 
 int main()
 {
-	char* cad1 = "original";
-	char* cad2 = "otra";
+	char *cad1 = "original";
+	char *cad2 = "otra";
 	char cad3[32];
 
 	copia(cad1,cad2);
@@ -39,9 +43,8 @@ int main()
 	printf("cad1 %s cad2 %s\n", cad1, cad2);
 
 	mod(cad1, cad3);
-	printf("cad1 %s cad3 %s\n", cad1,cad3);
+	printf("cad1 %s cad3 %s\n", cad1, cad3);
 
-	//mod(cad1, cad1);
+	mod(cad1, cad1);
 	return 0;
 }
-
