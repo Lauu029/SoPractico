448601fd4efb10b776cc087700310a8891c7a1aa
Laura Gomez <lgomez25@ucm.es>
2023-06-05 18:54:27 +0200

    Ejercicio5/Practica2

--- Laura/Practica2/api_ficheros_directorios/ejercicio5/espacio.c
+++ Laura/Practica2/api_ficheros_directorios/ejercicio5/espacio.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+#include <errno.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
 
 /* Forward declaration */
 int get_size_dir(char *fname, size_t *blocks);
@@ -9,9 +17,10 @@ int get_size_dir(char *fname, size_t *blocks);
  */
 int get_size(char *fname, size_t *blocks)
 {
-
+	int numBlocks = *blocks / 2;
+	printf("\t%dK %s\n", numBlocks, fname);
+	return numBlocks;
 }
-
 
 /* Gets the total number of blocks occupied by all the files in a directory. If
  * a contained file is a directory a recursive call to get_size_dir is
@@ -19,7 +28,52 @@ int get_size(char *fname, size_t *blocks)
  */
 int get_size_dir(char *dname, size_t *blocks)
 {
-
+	// Struct con la informacion que lee de los archivos dentro del directorio
+	/*
+	struct dirent {
+			   ino_t          d_ino;       Inode number
+			   off_t          d_off;       Not an offset; see below
+			   unsigned short d_reclen;    Length of this record
+			   unsigned char  d_type;      Type of file; not supported by all filesystem types
+			   char           d_name[256]; Null-terminated filename
+		   };
+	DT_BLK  block device.
+	DT_CHR  character device.
+	DT_DIR  directory.
+	DT_FIFO named pipe (FIFO).
+	DT_LNK  symbolic link.
+	DT_REG  regular file.s
+	DT_SOCK UNIX domain socket.
+	DT_UNKNOWN  The file type could not be determined.
+	*/
+	struct dirent *dirInfo;
+	struct stat statbuf;
+	DIR *d;
+	d = opendir(dname);
+	int t = statbuf.st_blocks / 2;
+	// Leo el directorio hasta el final del archivo
+	while ((dirInfo = readdir(d)) != NULL)
+	{
+		// Comparo el string del nombre con . y .. para ver si hay que ignorar el archivo
+		if ((strcmp(dirInfo->d_name, ".") != 0) & (strcmp(dirInfo->d_name, "..") != 0))
+		{
+			lstat(dirInfo->d_name, &statbuf);
+			switch (statbuf.st_mode & __S_IFMT)
+			{
+			case __S_IFDIR:
+				t += get_size_dir(dirInfo->d_name, &statbuf.st_blocks);
+				break;
+			case __S_IFREG:
+				t += get_size(dirInfo->d_name, &statbuf.st_blocks);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	printf("%dK %s\n", t, dname);
+	closedir(d);
+	return t;
 }
 
 /* Processes all the files in the command line calling get_size on them to
@@ -28,6 +82,24 @@ int get_size_dir(char *dname, size_t *blocks)
  */
 int main(int argc, char *argv[])
 {
+	int size;
+	struct stat statbuf;
+	// Reviso toda la lista de argumentos
+	for (int i = 1; i < argc; i++)
+	{
+		lstat(argv[i], &statbuf);
+		switch (statbuf.st_mode & __S_IFMT)
+		{
+		case __S_IFDIR:
+			size = get_size_dir(argv[i], &statbuf.st_blocks);
+			break;
+		case __S_IFREG:
+			size = get_size(argv[i], &statbuf.st_blocks);
+			break;
+		default:
+			break;
+		}
+	}
 
 	return 0;
 }
