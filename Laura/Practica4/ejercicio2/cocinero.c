3a780444f8e686ff503672fc0c1abb2157614df8
Laura Gomez <lgomez25@ucm.es>
2023-06-09 12:30:17 +0200

    ejercicio salvajes

--- Laura/Practica4/ejercicio2/cocinero.c
+++ Laura/Practica4/ejercicio2/cocinero.c
@@ -1,29 +1,70 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>	  /* For O_* constants */
+#include <signal.h>
 #define M 10
 
+sem_t *raciones;
+sem_t *vacias;
 int finish = 0;
+int *buffer;
 
 void putServingsInPot(int servings)
 {
+	int i;
+	int id = getpid();
+	for (i = 0; i < servings; i++)
+	{
+		*buffer+=1;
+		sem_post(raciones);
+	}
+	printf("All %d servings prepared by ID:%d\n", *buffer,id);
 }
 
 void cook(void)
 {
-	while(!finish) {
+	while (!finish)
+	{
 		putServingsInPot(M);
+		sem_wait(vacias);
 	}
 }
 
 void handler(int signo)
 {
 	finish = 1;
+	printf(" Closing program\n");
+	sem_post(vacias);
 }
 
 int main(int argc, char *argv[])
 {
+	//escucha las señales de consola
+	//(ctrl+c)->sigInt para leerlo si está bloqueado en un semáforo
+	//sigTerm-> es lo que se ejecuta al cerrar la consola(final de proceso)
+	signal(SIGINT, handler);
+	signal(SIGTERM, handler);
+	//id del fichero compartido y buffer de comida donde se almacena y se quita(int que guarda el valor)
+	int shared;
+	shared = shm_open("/BUFFER", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	ftruncate(shared, sizeof(int));
+	buffer = (int *)mmap(NULL, sizeof(int), PROT_WRITE, MAP_SHARED, shared, 0);
+	*buffer = 0;
+	raciones = sem_open("/RACIONES", O_CREAT, 0700, 0);
+	vacias = sem_open("/VACIAS", O_CREAT, 0700, 0);
 
+	cook();
+
+	munmap(buffer, sizeof(int));
+	shm_unlink("/BUFFER");
+	sem_close(raciones);
+	sem_close(vacias);
+	sem_unlink("/RACIONES");
+	sem_unlink("/VACIAS");
+	printf("Shared memory freed\n");
 	return 0;
 }
--- Laura/Practica4/ejercicio2/salvajes.c
+++ Laura/Practica4/ejercicio2/salvajes.c
@@ -1,28 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define NUMITER 15
 
-#define NUMITER 3
+sem_t *raciones;
+sem_t *vacias;
 
-int getServingsFromPot(void)
-{
-
-}
-
+int *buffer;
 void eat(void)
 {
-	unsigned long id = (unsigned long) getpid();
-	printf("Savage %lu eating\n", id);
+	unsigned long id = (unsigned long)getpid();
+	printf("Savage %lu eating\n", id, *buffer);
 	sleep(rand() % 5);
+}
+int getServingsFromPot(void)
+{
+	unsigned long id = (unsigned long)getpid();
+	if (*buffer == 0)
+	{
+		sem_post(vacias);
+	}
+	sem_wait(raciones);
+	*buffer -= 1;
+	printf("Savage %lu gets a serving, servings left:%d\n", id,*buffer);
 }
 
 void savages(void)
 {
-
+	for (int i = 0; i < NUMITER; i++)
+	{
+		getServingsFromPot();
+		eat();
+	}
 }
 
 int main(int argc, char *argv[])
 {
+	//Productor/consumidor
+	int shared;
+	if ((shared = shm_open("/BUFFER", O_RDWR,  S_IRUSR | S_IWUSR)) == -1)
+	{
+		printf("Error: Debes ejecutar primero el programa del cocinero\n");
+		return -1;
+	}
+	buffer = (int *)mmap(NULL, sizeof(int), PROT_WRITE, MAP_SHARED, shared, 0);
+	raciones = sem_open("/RACIONES", 0);
+	vacias = sem_open("/VACIAS", 0);
+	savages();
 
+	sem_close(raciones);
+	sem_close(vacias);
 	return 0;
 }
